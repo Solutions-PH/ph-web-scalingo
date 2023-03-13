bf4512887213e94a03b30e03db77ef6836a0d4b9
Edouard <edouard@7.fr>
2023-03-13 20:40:52 +0100

    Initial release

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
+
+$medooObject = [
+    "database_type" => "mssql",
+    "database_name" => "OSP_DATASTAT",
+    "server" => "51.178.76.132",
+    "username" => "edaubin",
+    "password" => "td5dakDN5u",
+];
+if ($_SERVER["SERVER_NAME"] != "atlas.test") {
+  //  $medooObject["driver"] = "dblib";
+}
+
+$myPDO = new \Medoo\Medoo($medooObject);
+
+$response = $myPDO->query("SELECT TOP 10 FROM ospharea_crm")->fetchAll(\PDO::FETCH_ASSOC);
+
+echo "<pre>";
+print_r($response[0]);
+
 phpinfo();
 ?>
