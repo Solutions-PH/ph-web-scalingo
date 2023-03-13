ace64fefbd4cc95f260026dc56710cd79b23c0e1
Edouard <edouard@7.fr>
2023-03-13 21:57:55 +0100

    Initial release

---
+++ index3.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once 'vendor/autoload.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+phpinfo();
+
+use Medoo\Medoo;
+
+$medooObject = [
+    "database_type" => "sybase",
+    "database_name" => "OSP_DATASTAT",
+    "server" => "51.178.76.132",
+    "username" => "edaubin",
+    "password" => "td5dakDN5u",
+];
+
+$myPDO = new \Medoo\Medoo($medooObject);
+
+$response = $myPDO->query("SELECT TOP 10 FROM ospharea_crm")->fetchAll(\PDO::FETCH_ASSOC);
+
+echo "<pre>";
+print_r($response[0]);
+
+phpinfo();
+?>
