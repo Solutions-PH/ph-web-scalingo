bd3877075ff8473b29ab1773c5524a344ea629de
Edouard <edouard@7.fr>
2023-03-15 12:47:11 +0100

    Initial release

--- index.php
+++ index.php
@@ -119,7 +119,7 @@ echo "<pre>";
 
 print_R(PDO::getAvailableDrivers());
 
-$d = scandir("/app/.apt/opt/microsoft/msodbcsql18/lib64/");
+$d = scandir("/app/.apt/opt/microsoft/msodbcsql17/lib64/");
 print_R($d);
 
 $d = scandir("/app/.apt/opt/microsoft/msodbcsql18/lib64/");
